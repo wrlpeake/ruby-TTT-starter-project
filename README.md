aa8340c107d341996e7e6d7fa07984ef2b7a650f
Will Peake <47351385+wrlpeake@users.noreply.github.com>
2022-08-19 12:29:02 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Ruby Tic-Tac-Toe Starter Project - Will Peake
+
+Detailed ReadMe to follow.
---
+++ spec/tic-tac-toe-spec.rb
@@ -0,0 +1,7 @@
+require_relative '../lib/tic-tac-toe.rb'
+
+describe Game do
+    it "has a passing test" do
+        expect(true).to equal true
+    end
+end
