aede4858c27280b619ea01d02837f05059b7b284
Will Peake <47351385+wrlpeake@users.noreply.github.com>
2022-08-23 19:21:30 +0100

    Updated with game instructions + board matrix
    Added simple instructions on how to play tic-tac-toe and have created a  3x3 matrix to act as the game board.

--- lib/tic-tac-toe.rb
+++ lib/tic-tac-toe.rb
@@ -1,8 +1,25 @@
+
 class Game
     def createWelcomeMessage
-        message = "Welcome to Tic-Tac-Toe (AKA Knoughts & Crosses)"
+        message = "Welcome to Tic-Tac-Toe (AKA Knoughts & Crosses)\n\n"
+    end
+
+    def createGameBoard
+        board = [
+                [1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]
+                ]
+    end
+
+    def displayInstructions
+        instructions = "This is a two-player game. The first player will be the 'X' team, the second player will be 'O' team.\n\n" +
+        "The aim of the game is to get three of your symbol in a row, taking in turns to select your spot on a 3x3 board.\n\n" +
+        "Choose numbers 1-9 to select your spot on the board\n\n";  
     end
 end
 
 newGame = Game.new
 puts newGame.createWelcomeMessage
+puts newGame.displayInstructions
+
--- spec/tic-tac-toe_spec.rb
+++ spec/tic-tac-toe_spec.rb
@@ -8,6 +8,23 @@ describe Game do
 
     it "should display a welcome message when the game is started" do
         testGame = Game.new
-        expect(testGame.createWelcomeMessage).to eql "Welcome to Tic-Tac-Toe (AKA Knoughts & Crosses)"
+        expect(testGame.createWelcomeMessage).to eql "Welcome to Tic-Tac-Toe (AKA Knoughts & Crosses)\n\n"
     end 
+
+    it "should contain a 3x3 matrix to act as the game board" do
+        testGame = Game.new
+        expect(testGame.createGameBoard).to eql [
+                                                [1, 2, 3],
+                                                [4, 5, 6],
+                                                [7, 8, 9]
+                                                ]
+    end
+
+    it "should display simple instructions explaining how to play when the game is started" do
+        testGame = Game.new
+        expect(testGame.displayInstructions).to eql "This is a two-player game. The first player will be the 'X' team, the second player will be 'O' team.\n\n" +
+                                                    "The aim of the game is to get three of your symbol in a row, taking in turns to select your spot on a 3x3 board.\n\n" +
+                                                    "Choose numbers 1-9 to select your spot on the board\n\n";
+    end
 end 
+
