67bd8f37d5f1ca384446bf483b2cd2e68d927cae
Will Peake <47351385+wrlpeake@users.noreply.github.com>
2022-08-25 15:27:17 +0100

    Update to implement Player 'X'
    Added features to:
    
    - Request an integer between 1-9 from player 'x'
    
    - Use that integer to 'mark' the board

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,27 +1,90 @@
 
 class Game
+
+    game_board = [[1, 2, 3],
+                  [4, 5, 6],
+                  [7, 8, 9]]
+
     def create_welcome_message
         message = "Welcome to Tic-Tac-Toe (AKA Knoughts & Crosses)\n\n"
-    end
-
-    def create_game_board
-        board = [
-                [1, 2, 3],
-                [4, 5, 6],
-                [7, 8, 9]
-                ]
+        puts message
+        message
     end
 
     def display_instructions
         instructions = "This is a two-player game. The first player will be the 'X' team, the second player will be 'O' team.\n\n" +
         "The aim of the game is to get three of your symbol in a row, taking in turns to select your spot on a 3x3 board.\n\n" +
-        "Choose numbers 1-9 to select your spot on the board\n\n";  
+        "Choose numbers 1-9 to select your spot on the board\n\n";
+        puts instructions
+        instructions  
+    end
+
+    def display_game_board(game_board)  
+        puts game_board.to_a.map(&:inspect)
+        game_board
+    end 
+
+    def get_player_x_selection
+        puts "Player 1 AKA 'X' - Choose an integer between 1-9 to place your 'X' on the board"
+        player_x_selection = gets.to_i
+            if !(player_x_selection >= 1 && player_x_selection <=9)
+                puts "Incorrect selection, please select a number between 1-9"
+                get_player_x_selection
+            else
+                print "Player X selected position: "
+                puts player_x_selection
+                player_x_selection
+            end        
+    end
+
+    def mark_game_board(game_board, get_player_x_selection)        
+        if get_player_x_selection == 1
+            game_board[0][0] = "X"
+            puts game_board.to_a.map(&:inspect)
+            game_board[0][0]
+        elsif
+            get_player_x_selection == 2
+            game_board[0][1] = "X"
+            puts game_board.to_a.map(&:inspect)
+            game_board[0][1]
+        elsif
+            get_player_x_selection == 3
+            game_board[0][2] = "X"
+            puts game_board.to_a.map(&:inspect)
+            game_board[0][2]
+        elsif
+            get_player_x_selection == 4
+            game_board[1][0] = "X"
+            puts game_board.to_a.map(&:inspect)
+            game_board[1][0]
+        elsif
+            get_player_x_selection == 5
+            game_board[1][1] = "X"
+            puts game_board.to_a.map(&:inspect)
+            game_board[1][1]
+        elsif
+            get_player_x_selection == 6
+            game_board[1][2] = "X"
+            puts game_board.to_a.map(&:inspect)
+            game_board[1][2]
+        elsif
+            get_player_x_selection == 7
+            game_board[2][0] = "X"
+            puts game_board.to_a.map(&:inspect)
+            game_board[2][0]
+        elsif
+            get_player_x_selection == 8
+            game_board[2][1] = "X"
+            puts game_board.to_a.map(&:inspect)
+            game_board[2][1]
+        else
+            get_player_x_selection == 9
+            game_board[2][2] = "X"
+            puts game_board.to_a.map(&:inspect)
+            game_board[2][2]
+        end
     end
 end
 
 NewGame = Game.new
-puts NewGame.create_welcome_message
-puts NewGame.display_instructions
-puts NewGame.create_game_board.to_a.map(&:inspect)
-
 
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -7,24 +7,29 @@ describe Game do
         expect(TestGame.create_welcome_message).to eql "Welcome to Tic-Tac-Toe (AKA Knoughts & Crosses)\n\n"
     end 
 
-    it "should contain a 3x3 matrix to act as the game board" do
-        expect(TestGame.create_game_board).to eql [
-                                                [1, 2, 3],
-                                                [4, 5, 6],
-                                                [7, 8, 9]
-                                                ]
-    end
-
     it "should display simple instructions explaining how to play when the game is started" do
         expect(TestGame.display_instructions).to eql "This is a two-player game. The first player will be the 'X' team, the second player will be 'O' team.\n\n" +
                                                     "The aim of the game is to get three of your symbol in a row, taking in turns to select your spot on a 3x3 board.\n\n" +
                                                     "Choose numbers 1-9 to select your spot on the board\n\n";
     end
 
-    it "should request an integer between 1-9 from the human player" do
-        
+    it "should show the user a 3x3 board with numbers 1-9" do
+        game_board = [[1, 2, 3],
+                      [4, 5, 6],
+                      [7, 8, 9]]
+        expect(TestGame.display_game_board(game_board)).to eql [[1, 2, 3],
+                                                                [4, 5, 6],
+                                                                [7, 8, 9]]
+    end
 
+    it "should request and receive an integer between 1-9 from the the first player, player x" do
+        expect(TestGame.get_player_x_selection).to be_between(1,9).inclusive
+    end
+
+    it "should allow player x to mark the board via by choosing an integer" do
+        game_board = [[1, 2, 3],
+                      [4, 5, 6],
+                      [7, 8, 9]]
+        expect(TestGame.mark_game_board(game_board, TestGame.get_player_x_selection)).to eql "X"
     end
 end 
-
-
