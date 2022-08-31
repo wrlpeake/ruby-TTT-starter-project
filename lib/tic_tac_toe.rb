66c5442388f45acae82f997f25059bccc593c699
Will Peake <47351385+wrlpeake@users.noreply.github.com>
2022-08-31 16:53:20 +0100

    Refactored to remove duplication of code + simpler structure
    Changed from nested array to standard array to make changes simpler. Updated player_selection method so it can be used for both players, to remove duplication. Corrected rubocop warnings.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,76 +1,64 @@
 # frozen_string_literal: true
 
 class TicTacToe
-  game_board = [[1, 2, 3],
-                [4, 5, 6],
-                [7, 8, 9]]
+  @game_board = %w[1 2 3 4 5 6 7 8 9]
 
   def display_welcome_message
-    message = 'Welcome to Tic-Tac-Toe (AKA Knoughts & Crosses)\n'
-    message
+    'Welcome to Tic-Tac-Toe (AKA Knoughts & Crosses)\n'
   end
 
   def display_instructions
-    instructions = "This is a two-player game. The first player will be the 'X' team, the second player will be 'O' team.\n\n" \
-                   "The aim of the game is to get three of your symbol in a row, taking in turns to select your spot on a 3x3 board.\n\n" \
-                   "Choose numbers 1-9 to select your spot on the board\n\n"
-    instructions
+    "This is a two-player game. The first player will be the 'X' team, the second player will be 'O' team.\n\n" \
+    "The aim of the game is to get three of your symbol in a row, taking in turns to select your spot on a 3x3 board.\n\n" \
+    "Choose numbers 1-9 to select your spot on the board\n\n"
   end
 
   def display_game_board(game_board)
-    game_board
-  end
-
-  def request_player_x_selection
-    puts "Player 1 AKA 'X' - Choose an integer between 1-9 to place your 'X' on the board"
-    player_x_selection = gets.to_i
-    if !(player_x_selection >= 1 && player_x_selection <= 9)
-      get_player_x_selection
-    else
-      print 'Player X selected position: '
-      player_x_selection
+    horizontal_board_line = '----------'
+    game_board.each do |i|
+      print game_board[i - 1] if (i % 3) == 1
+      if (i % 3) == 2
+        print ' | '
+        print game_board[i - 1]
+        print ' | '
+      end
+      next unless (i % 3).zero?
+      print game_board[i - 1]
+      print "\n"
+      puts horizontal_board_line
     end
   end
 
-  def mark_game_board(game_board, get_player_x_selection)
-    case get_player_x_selection
-    when 1
-      game_board[0][0] = 'X'
-      puts game_board.to_a.map(&:inspect)
-      game_board[0][0]
-    when 2
-      game_board[0][1] = 'X'
-      puts game_board.to_a.map(&:inspect)
-      game_board[0][1]
-    when 3
-      game_board[0][2] = 'X'
-      puts game_board.to_a.map(&:inspect)
-      game_board[0][2]
-    when 4
-      game_board[1][0] = 'X'
-      puts game_board.to_a.map(&:inspect)
-      game_board[1][0]
-    when 5
-      game_board[1][1] = 'X'
-      puts game_board.to_a.map(&:inspect)
-      game_board[1][1]
-    when 6
-      game_board[1][2] = 'X'
-      puts game_board.to_a.map(&:inspect)
-      game_board[1][2]
-    when 7
-      game_board[2][0] = 'X'
-      puts game_board.to_a.map(&:inspect)
-      game_board[2][0]
-    when 8
-      game_board[2][1] = 'X'
-      puts game_board.to_a.map(&:inspect)
-      game_board[2][1]
+  def request_player_selection(which_player)
+    puts "Player " + which_player + ", please choose an integer between 1 and 9."
+    player_selection = gets.to.i
+    if !(player_selection >= 1 && player_selection <= 9)
+      puts "Error: not an integer between 1 and 5. Please choose again."
+      request_player_selection
+    elsif check_game_board(game_board, player_selection) == true
+      puts "Player " + which_player + ", has selected: " + player_selection
+      player_selection
     else
-      get_player_x_selection == 9
-      game_board[2][2] = 'X'
-      puts game_board.to_a.map(&:inspect)
-      game_board[2][2]
+      puts "Error: already selected. Please choose again."
+      request_player_selection
+    end
+  end
+
+  def check_game_board(game_board, player_selection)
+    if game_board[player_selection - 1] == 'X'
+      false
+    elsif game_board[player_selection - 1] == 'O'
+      false
+    else
+      true
+    end
+  end
+
+  def mark_game_board(game_board, player_selection, which_player)
+    if which_player == 'X'
+      game_board[player_selection - 1] = 'X'
+    else
+      game_board[player_selection - 1] = 'O'
     end
   end
 end
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -2,40 +2,52 @@
 
 require_relative '../lib/tic_tac_toe'
 
+TestGame = TicTacToe.new
+
 describe TicTacToe do
+
   it 'should display a welcome message when the game is started' do
-    TestGame = TicTacToe.new
     expect(TestGame.display_welcome_message).to eql 'Welcome to Tic-Tac-Toe (AKA Knoughts & Crosses)\n'
   end
 
   it 'should display simple instructions explaining how to play when the game is started' do
     expect(TestGame.display_instructions).to eql "This is a two-player game. The first player will be the 'X' team, the second player will be 'O' team.\n\n" \
-                                                 "The aim of the game is to get three of your symbol in a row, taking in turns to select your spot on a 3x3 board.\n\n" \
-                                                 "Choose numbers 1-9 to select your spot on the board\n\n"
+                                "The aim of the game is to get three of your symbol in a row, taking in turns to select your spot on a 3x3 board.\n\n" \
+                                "Choose numbers 1-9 to select your spot on the board\n\n"
   end
 
   it 'should show the user a 3x3 board with numbers 1-9' do
-    game_board = [[1, 2, 3],
-                  [4, 5, 6],
-                  [7, 8, 9]]
-    expect(TestGame.display_game_board(game_board)).to eql [[1, 2, 3],
-                                                            [4, 5, 6],
-                                                            [7, 8, 9]]
+    game_board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    expect(TestGame.display_game_board(game_board)).to eql [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
-  it 'should request and receive an integer between 1-9 from the the first player, player x' do
-    request_player_x_selection = 4
-    expect(request_player_x_selection).to be_between(1, 9).inclusive
+  it 'should request and receive an integer between 1-9 from the player' do
+    request_player_selection = 4
+    expect(request_player_selection).to be_between(1, 9).inclusive
     # still considering how to structure this test for user input
   end
 
   it 'should allow player x to mark the board via by choosing an integer' do
-    game_board = [[1, 2, 3],
-                  [4, 5, 6],
-                  [7, 8, 9]]
-    player_x_selection = 7
-    TestGame.mark_game_board(game_board, player_x_selection)
-    expect(game_board[2][0]).to eql 'X'
+    game_board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    player_selection = 7
+    which_player = 'X'
+    TestGame.mark_game_board(game_board, player_selection, which_player)
+    expect(game_board[6]).to eql 'X'
+  end
+
+  it 'should allow player o to mark the board via by choosing an integer' do
+    game_board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    player_selection = 5
+    which_player = 'O'
+    TestGame.mark_game_board(game_board, player_selection, which_player)
+    expect(game_board[4]).to eql 'O'
+  end
+
+  it 'should not let a player choose a spot that has already been selected' do
+    game_board = [1, 2, 3, 4, 5, 6, 7, 'X', 9]
+    player_selection = 8
+    expect(TestGame.check_game_board(game_board, player_selection)).to be false
   end
 end
+
 
